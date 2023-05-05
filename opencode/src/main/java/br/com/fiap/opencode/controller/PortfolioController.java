905e69e56607e89e5479f6e6536fa2a7ea2e823b
Luiz Felipe <103139855+LuizSdO@users.noreply.github.com>
2023-05-05 10:22:46 -0300

    hateoas

--- opencode/src/main/java/br/com/fiap/opencode/controller/PortfolioController.java
+++ opencode/src/main/java/br/com/fiap/opencode/controller/PortfolioController.java
@@ -1,5 +1,6 @@
 package br.com.fiap.opencode.controller;
 
+import br.com.fiap.opencode.model.Codigo;
 import br.com.fiap.opencode.model.Portfolio;
 import br.com.fiap.opencode.repository.PortfolioRepository;
 import org.slf4j.Logger;
@@ -8,10 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,25 +26,31 @@ public class PortfolioController {
         Logger log = LoggerFactory.getLogger(br.com.fiap.opencode.controller.PortfolioController.class);
         @Autowired
         PortfolioRepository repository;
+        @Autowired
+        PagedResourcesAssembler<Object> assembler;
 
         @GetMapping("/api/portfolio")
-        public Page<Portfolio> index(@PageableDefault(size = 5) Pageable pageable){
-            return repository.findAll(pageable);
+        public PagedModel<EntityModel<Object>> index(@RequestParam(required = false) String busca, @PageableDefault(size = 5) Pageable pageable){
+            Page<Portfolio> portfolios = repository.findAll(pageable);
+            return assembler.toModel(portfolios.map(Portfolio::toEntityModel));
         }
 
     @PostMapping("/api/portfolio")
     public ResponseEntity<Portfolio> create(@RequestBody Portfolio portfolio) {
         log.info("cadastrando portfolio: " + portfolio);
         repository.save(portfolio);
-        return ResponseEntity.status(HttpStatus.CREATED).body(portfolio);
+        portfolio.setUsuario(repository.findById(portfolio.getUsuario().getId()).get().getUsuario());
+        return ResponseEntity
+                .created(portfolio.toEntityModel().getRequiredLink("self").toUri())
+                .body(portfolio.toEntityModel().getContent());
     }
 
     @GetMapping("api/portfolio/{id}")
-    public ResponseEntity<Portfolio> show(@PathVariable Long id) {
+    public EntityModel<Portfolio> show(@PathVariable Long id) {
         log.info("Buscar portfolio " + id);
-        var portfolioEncontrada = repository.findById(id)
+        var portfolio = repository.findById(id)
                 .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Portfolio não Encontrado"));
-        return ResponseEntity.ok(portfolioEncontrada);
+        return portfolio.toEntityModel();
     }
 
     @DeleteMapping("/api/portfolio/{id}")
@@ -50,12 +62,12 @@ public class PortfolioController {
     }
 
     @PutMapping("/api/portfolio/{id}")
-    public ResponseEntity<Portfolio> update(@PathVariable Long id, @RequestBody Portfolio portfolio) {
+    public EntityModel<Portfolio> update(@PathVariable Long id, @RequestBody Portfolio portfolio) {
         var portfolioEncontrada = repository.findById(id)
                 .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Portfolio não Encontrado"));
         portfolio.setId(id);
         repository.save(portfolioEncontrada);
-        return ResponseEntity.ok(portfolio);
+        return portfolio.toEntityModel();
     }
 
     }
--- opencode/src/main/java/br/com/fiap/opencode/model/Portfolio.java
+++ opencode/src/main/java/br/com/fiap/opencode/model/Portfolio.java
@@ -1,10 +1,17 @@
 package br.com.fiap.opencode.model;
 
+import br.com.fiap.opencode.controller.UsuarioController;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
+import br.com.fiap.opencode.controller.PortfolioController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Data
 @AllArgsConstructor
@@ -25,5 +32,15 @@ public class Portfolio {
     public Portfolio(String portfolio, String nomePortfolio) {
         this.portfolio = portfolio;
         this.nomePortfolio = nomePortfolio;
+    }
+
+    public EntityModel<Portfolio> toEntityModel() {
+        return EntityModel.of(
+                this,
+                linkTo(methodOn(PortfolioController.class).show(id)).withSelfRel(),
+                linkTo(methodOn(PortfolioController.class).delete(id)).withRel("delete"),
+                linkTo(methodOn(PortfolioController.class).index(null, Pageable.unpaged())).withRel("all"),
+                linkTo(methodOn(UsuarioController.class).show(this.getUsuario().getId())).withRel("usuario")
+        );
     }
 }
